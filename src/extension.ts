a0e69f5aa7cf89a7121a904b967184da7bdfad28
saber2pr <saber2pr@gmail.com>
2021-05-04 13:31:34 +0800

    docs: update readme
    docs: update
    
    update

--- src/extension.ts
+++ src/extension.ts
@@ -3,27 +3,27 @@ import { join } from 'path';
 import { promisify } from 'util';
 import * as vscode from 'vscode';
 
-const indexFileName = 'index'
-const indexFileExt = '.ts'
+const indexFileName = 'index';
+const indexFileExt = '.ts';
 
 export const createIndexFileContent = (moduleNames: string[]) =>
-	moduleNames.map(n => `export * from './${n}';`).join('\n')
+	moduleNames.map(n => `export * from './${n}';`).join('\n');
 
 export async function genIndex(dir: string) {
-	const result = await promisify(readdir)(dir)
+	const result = await promisify(readdir)(dir);
 
 	const moduleNames = result
 		.map(n => n.split('.')[0])
-		.filter(m => m !== indexFileName)
+		.filter(m => m !== indexFileName);
 
-	const content = createIndexFileContent(moduleNames)
+	const content = createIndexFileContent(moduleNames);
 
-	await promisify(writeFile)(join(dir, indexFileName + indexFileExt), content)
+	await promisify(writeFile)(join(dir, indexFileName + indexFileExt), content);
 }
 
 export function activate(context: vscode.ExtensionContext) {
 	const createIndexCommand = vscode.commands.registerCommand('createdirindex', (uri: vscode.Uri) => {
-		genIndex(uri.fsPath)
+		genIndex(uri.fsPath);
 	});
 
 	context.subscriptions.push(createIndexCommand);
